dbbb55f20bd3ace058a1fe71ab72efe8c67d5531
Brain <Brain@localhost>
2014-07-25 21:04:44 +0200

    assignment 2 commit 1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These 2 functions implement the solution to R Rpogramming Assignment 2
 
-## Write a short comment describing this function
+## makeCacheMatrix stores a matrix and be used to return the matrix or its inverse
+## Example usage: m <- makeCacheMatrix(matrix(c(1,2,3,4),2,2))
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse_m <- NULL
+  set <- function(y) {
+    x <<- y
+    inverse_m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) inverse_m <<- solve
+  getinverse <- function() inverse_m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse) 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes a datastructure created by makeCacheMatrix and returns it inverse
+## Example usage: cacheSolve(m)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
